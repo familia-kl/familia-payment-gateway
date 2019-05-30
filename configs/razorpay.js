f835698c482f78b415735aa73536d6ce612df43c
akhilkm <akhilu011@gmail.com>
2019-05-30 11:57:06 +0530

    updated

---
+++ configs/razorpay.js
@@ -0,0 +1,4 @@
+module.exports = {
+    razorpayKeyId: (process.env.RAZORPAY_KEY_ID || 'rzp_test_UxIGhWEX3CvQrq'),
+    razorpayKeySecret: (process.env.RAZORPAY_KEY_SECRET  || 'bhbCMBrA5sJ2ugqHNw50NUVy'),
+}
